a4f48891c558c0bb1ce7509bc6f523bbf2a071d3
Pogs789 <cesphillipmiraflor.lorica@bicol-u.edu.ph>
2024-10-05 09:43:09 +0800

    upgraded the security feature and modified the password controller for employee id and password

--- app/Http/Controllers/Auth/ConfirmablePasswordController.php
+++ app/Http/Controllers/Auth/ConfirmablePasswordController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
 
@@ -24,12 +25,45 @@ class ConfirmablePasswordController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        if (! Auth::guard('web')->validate([
-            'employee_number' => $request->user()->employee_number,
-            'password' => $request->password,
-        ])) {
+        $employeeId = $request->input('employee_id');
+        $password = $request->input('password');
+
+        $temp = explode('-', $employeeId);
+        if(count($temp) != 2){
             throw ValidationException::withMessages([
-                'password' => __('auth.password'),
+                'employee_id' => _('You entered the wrong employee Id. Please Try Again.')
+            ]);
+        }
+
+        $aphso_division = $temp[0];
+        $emp_no = $temp[1];
+
+        $aphso_division = DB::table('aphso_division')->where('division_name', $aphso_division)->first();
+        if(!$aphso_division){
+            throw ValidationException::withMessages([
+                'employee_id' => _('The Divison you entered is invalid. Please Try Again.')
+            ]);
+        }
+
+        $employee = DB::table('aphso_employees')
+            ->where('employee_number', $emp_no)
+            ->where('division_id')
+            ->first();
+        if(!$employee){
+            throw ValidationException::withMessages([
+                'employee_id' => _('Employee number is invalid. Please Try Again.')
+            ]);
+        }
+
+        $credentials = [
+            'employee_no' => $employeeId,
+            'password' => $password
+        ];
+
+        if(!Auth::guard('web')->attempt($credentials)){
+            throw ValidationException::withMessages([
+                'password' => ('auth.password'),
+                'employee_id' => _('Wrong Employee Id and/or Password. Please Try Again.')
             ]);
         }
 
