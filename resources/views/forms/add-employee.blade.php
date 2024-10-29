e1c2cf79e0ae3c5f3aebd502ebabf824afe8a60b
Pogs789 <cesphillipmiraflor.lorica@bicol-u.edu.ph>
2024-10-29 10:21:29 +0800

    edited the personal information of the employee form, added a progress bar

--- resources/views/forms/add-employee.blade.php
+++ resources/views/forms/add-employee.blade.php
@@ -1,12 +1,27 @@
 <x-modal name="add-new-employee" :show="false" focusable>
+
+    <!--Close Modal-->
     <div class = "pt-10 pb-4 py-4">
-        <button class = "justify-end items-end right-5 absolute" x-on:click.prevent="$dispatch(close-modal)">
+        <button class = "justify-end items-end right-5"  x-on:click.prevent="$dispatch('close-modal', 'add-new-employee')">
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M7.05086 5.63616C6.66033 5.24563 6.02717 5.24563 5.63664 5.63616C5.24612 6.02668 5.24612 6.65984 5.63664 7.05037L10.5864 12.0001L5.63664 16.9499C5.24612 17.3404 5.24612 17.9736 5.63664 18.3641C6.02717 18.7546 6.66033 18.7546 7.05086 18.3641L12.0006 13.4143L16.9504 18.3641C17.3409 18.7546 17.974 18.7546 18.3646 18.3641C18.7551 17.9736 18.7551 17.3404 18.3646 16.9499L13.4148 12.0001L18.3646 7.05037C18.7551 6.65984 18.7551 6.02668 18.3646 5.63616C17.974 5.24563 17.3409 5.24563 16.9504 5.63616L12.0006 10.5859L7.05086 5.63616Z" fill="black"/>
                 </svg>
         </button>
         <div class = "relative">
             <h3 class = "text-center font-bold text-2xl">Add New Employee</h3>
+
+            <!--Progress Bar-->
+            <div class = "inline-flex p-5 justify-between">
+                <div class = "rounded-full h-[60px] w-[60px] border-[#7fbaff] bg-[#7fbaff] text-center p-4">1</div>
+                <div class="w-[491px] h-[18px] flex-col justify-start items-start gap-1 inline-flex">
+                    <div class="h-3 pr-[489.69px] bg-[#eff1f4] rounded-[20px] justify-start items-center inline-flex absolute">
+                        <div class="grow shrink basis-0 h-3 bg-white"></div>
+                    </div>
+                </div>
+                <div class = "rounded-full h-[60px] w-[60px] border-gray-300 bg-gray-300 text-center p-4">2</div>
+            </div>
+
+            <!--Form Part 1-->
             <form action="{{route('employees.add')}}" method = "POST" class = "p-4">
                 @csrf
                 <h5 class = "font-bold text-xl">Part I: Personal Information</h5>
